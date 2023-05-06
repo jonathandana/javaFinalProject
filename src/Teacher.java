f6a974c882234d3e5107087002f888df8a54ba36
Jonathan Dana <jonathandana27@gmail.com>
2023-05-06 15:19:13 +0300

    add new class teacher

---
+++ src/Teacher.java
@@ -0,0 +1,23 @@
+public class Teacher extends Person {
+    private double wage;
+
+    public void  input(){
+        super.input();
+        String enter_wage = "Enter the teacher's wage: ";
+        System.out.print(enter_wage);
+
+        while (!in.hasNextDouble()) {
+            System.out.println("Invalid input!");
+            System.out.print(enter_wage);
+            in.nextLine(); // Consume the invalid input
+        }
+        wage = in.nextDouble();
+
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s Wage: %.2f", super.toString(), wage);
+    }
+}
